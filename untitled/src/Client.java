273ea9968a7188334d36310a055276e790b5cb31
PedroVictorAndreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2024-09-06 15:01:42 -0300

    Implementado melhor de 3

--- untitled/src/Client.java
+++ untitled/src/Client.java
@@ -15,7 +15,8 @@ public class Client{
 class tcpClient{
     public void run() throws Exception{
         Scanner scanner = new Scanner(System.in);
-        int res = 0;
+        int res = 0,cont = 0;
+        String s,s2= "",s1;
 
         Socket socket = new Socket("127.0.0.1",5000);
         Vector<Integer> vec = new Vector<Integer>(3);
@@ -24,20 +25,25 @@ class tcpClient{
         vec.add(3);
         DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-        while (!vec.contains(res)) {
-            System.out.println("Informe: \n1 - para papel\n2 - para pedra\n3 - para tesoura");
-            res = scanner.nextInt();
-            if (vec.contains(res))
-                out.writeInt(res);
-            else
-                System.out.println("numero invalido");
+        while (cont <3) {
+            while (!vec.contains(res)) {
+                System.out.println("Informe: \n1 - para papel\n2 - para pedra\n3 - para tesoura");
+                res = scanner.nextInt();
+                if (vec.contains(res))
+                    out.writeInt(res);
+                else
+                    System.out.println("numero invalido");
+            }
+            System.out.println("Aguardando jogador 1");
+            s = in.readUTF();
+            s1 = in.readUTF();
+            cont = in.readInt();
+            s2 = in.readUTF();
+            System.out.println(s);
+            System.out.println(s1);
+            res = 0;
         }
-        System.out.println("Aguardando jogador 1");
-        String s = in.readUTF();
-        String s1 = in.readUTF();
-        System.out.println(s);
-        System.out.println(s1);
-
+        System.out.println(s2);
         in.close();
         out.close();
         socket.close();
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -16,8 +16,8 @@ class tcpServer{
     public void run() throws Exception{
         ServerSocket serverSocket = new ServerSocket(5000);
         Scanner scanner = new Scanner(System.in);
-        int res = 0;
-        String s ="",s1 ="",resJodador1,resJogador2;
+        int res = 0,cont = 0,pontosJogador1 =0,pontosJogador2 = 0;
+        String s ="",s1 ="",s2 = "",resJodador1,resJogador2;
         System.out.println("Aguardando Joagador 2 ...");
 
         Socket socket = serverSocket.accept();
@@ -29,50 +29,77 @@ class tcpServer{
 
         DataInputStream in = new DataInputStream(socket.getInputStream());
         DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        while (cont<3) {
+            while (!vec.contains(res)) {
+                System.out.println("Informe: \n1 - para papel\n2 - para pedra\n3 - para tesoura");
+                res = scanner.nextInt();
+                if (!vec.contains(res))
+                    System.out.println("numero invalido");
+            }
+            System.out.println("Aguardando jogador 2");
+            int k = in.readInt();
+            if (res == 1)
+                resJodador1 = "Jogador 1 jogou papel";
+            else if (res == 2)
+                resJodador1 = "Jogador 1 jogou pedra";
+            else
+                resJodador1 = "Jogador 1 jogou tesoura";
 
-        while (!vec.contains(res)) {
-            System.out.println("Informe: \n1 - para papel\n2 - para pedra\n3 - para tesoura");
-            res = scanner.nextInt();
-            if (!vec.contains(res))
-                System.out.println("numero invalido");
+            if (k == 1)
+                resJogador2 = "Jogador 2 jogou papel";
+            else if (k == 2)
+                resJogador2 = "Jogador 2 jogou pedra";
+            else
+                resJogador2 = "Jogador 2 jogou tesoura";
+            s1 = resJodador1 + " e " + resJogador2;
+
+            if (res == k)
+                s = "empate";
+            else if (res == 2 && k == 3){
+                s = "jogador 1 ganha";
+                pontosJogador1 +=1;
+            }
+            else if (res == 3 && k == 2){
+                s = "jogador 2 ganha";
+                pontosJogador2 +=1;
+            }
+            else if (res == 1 && k == 2){
+                s = "jogador 1 ganha";
+                pontosJogador1 +=1;
+            }
+            else if (res == 2 && k == 1){
+                s = "jogador 2 ganha";
+                pontosJogador2 +=1;
+            }
+            else if (res == 3 && k == 1){
+                s = "jogador 1 ganha";
+                pontosJogador1 +=1;
+            }
+            else{
+                s = "jogador 2 ganha";
+                pontosJogador2 +=1;
+            }
+
+
+
+            cont += 1;
+            if(cont == 3){
+                if(pontosJogador1 == pontosJogador2)
+                    s2 = "empate no total";
+                else if (pontosJogador1>pontosJogador2)
+                    s2 = "Jogador 1 venceu a melhor de tres";
+                else
+                    s2 = "Jogador 2 venceu a melhor de tres";
+            }
+            System.out.println(s1);
+            System.out.println(s);
+            out.writeUTF(s1);
+            out.writeUTF(s);
+            out.writeInt(cont);
+            out.writeUTF(s2);
+            res = 0;
         }
-        int k = in.readInt();
-        System.out.println("Aguardando jogador 2");
-        if(res == 1)
-            resJodador1 = "Jogador 1 jogou papel";
-        else if (res == 2)
-            resJodador1 = "Jogador 1 jogou pedra";
-        else
-            resJodador1 = "Jogador 1 jogou tesoura";
-
-        if(k == 1)
-            resJogador2 = "Jogador 2 jogou papel";
-        else if (k == 2)
-            resJogador2 = "Jogador 2 jogou pedra";
-        else
-            resJogador2 = "Jogador 2 jogou tesoura";
-        s1 = resJodador1 + " e " + resJogador2;
-
-        if(res == k)
-            s = "empate";
-        else if (res == 2 && k== 3)
-            s = "jogador 1 ganha";
-        else if (res == 3 && k== 2)
-            s = "jogador 2 ganha";
-        else if (res == 1 && k== 2)
-            s = "jogador 1 ganha";
-        else if (res == 2 && k== 1)
-            s = "jogador 2 ganha";
-        else if (res == 3 && k== 1)
-            s = "jogador 1 ganha";
-        else
-            s = "jogador 2 ganha";
-
-        System.out.println(s1);
-        System.out.println(s);
-        out.writeUTF(s1);
-        out.writeUTF(s);
-
+        System.out.println(s2);
         in.close();
         out.close();
         socket.close();
